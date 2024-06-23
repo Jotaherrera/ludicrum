b3c005ebd652511c5195cf54ef74efd2f65251cd
Juan Herrera <juanherreramen13@gmail.com>
2024-06-23 12:05:15 -0500

    chore: add terraform apply for github infra (#1)
    - add prod workflow
    - add terraform apply job for github infra

---
+++ common/infrastructure/jobs/apply_github_infrastructure.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source "$(dirname "$0")"/../../utils/logs.sh
+
+function apply_infrastructure {
+  : && log_loading "Applying GitHub infrastructure configuration with Terraform" \
+    && log_info "Moving to GitHub infrastructure directory" \
+    && cd common/infrastructure/github \
+    && log_loading "Initiating Terraform" \
+    && terraform init \
+    && log_success "Terraform correctly initialized" \
+    && log_loading "Importing current infrastructure configuration" \
+    && terraform import github_repository.ludicrum ludicrum \
+    && log_success "Current configuration imported successfully" \
+    && log_loading "Applying infrastructure changes" \
+    && if terraform apply -auto-approve; then
+      log_success "GitHub infrastructure configuration has been applied correctly"
+    else
+      log_failure "Something went wrong while applying the infrastructure" \
+        && exit 1
+    fi
+}
+
+function main {
+  : && apply_infrastructure
+}
+
+main "${@}"
